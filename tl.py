0b05984cc664d11895c914af44c5cd726c20b702
ctcarrier <ctcarrier@gmail.com>
2016-03-25 13:36:24 -0400

    Bug fix

--- tl.py
+++ tl.py
@@ -180,9 +180,9 @@ class App():
             prev_acquired = None
             last_acquired = None
             last_started = None
+            been_over = False
 
             while True:
-                beenOver = False
                 last_started = datetime.now()
                 config = CONFIGS[current_config]
                 logging.info("Shot: %d T: %s ISO: %d" % (self.shot, config[1], config[3]))
@@ -209,20 +209,21 @@ class App():
                 last_acquired = datetime.now()
                 persist.writeLastConfig(current_config, self.shot, brightness, SETTINGS_FILE, flash_on)
 
-                logging.info("Shot: %d File: %s Brightness: %s Flash: %s Been Over: %s" % (self.shot, filename, brightness, flash_on, beenOver))
+                logging.info("Shot: %d File: %s Brightness: %s Flash: %s Been Over: %s" % (self.shot, filename, brightness, flash_on, been_over))
 
-                if brightness < MIN_BRIGHTNESS and current_config < len(CONFIGS) - 1 and beenOver == False:
+                if brightness < MIN_BRIGHTNESS and current_config < len(CONFIGS) - 1 and been_over == False:
                     logging.info("Under not been over")
                     if (flash_on == False and current_config >= FLASH_THRESHOLD):
                         flash_on = True
                     else: current_config = current_config + 1
                 elif brightness > MAX_BRIGHTNESS and current_config > 0:
                     logging.info("Over")
-                    beenOver = True
+                    been_over = True
                     if (flash_on == True and current_config < FLASH_THRESHOLD):
                         flash_on = False
                     else: current_config = current_config - 1
                 else:
+                    been_over = False
                     self.shot += 1
                     if last_started and last_acquired and last_acquired - last_started < MIN_INTER_SHOT_DELAY_SECONDS:
                         logging.info("Sleeping for %s" % str(MIN_INTER_SHOT_DELAY_SECONDS - (last_acquired - last_started)))
